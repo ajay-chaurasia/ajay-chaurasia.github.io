4ca3a52a711d5d72c4acbf4274c1aea632234626
Ajay Chaurasia <ajay.chaurasia29@gmail.com>
2022-07-04 15:08:04 +0530

    Adding summary section

--- ajay.js
+++ ajay.js
@@ -1,9 +1,130 @@
 const ajay = {
-    "summary": [
+    education: [
+        {
+            degree: "Bachelor of Engineering",
+            course: "Computer Science",
+            institution: "ITM Universe, Gwalior (M.P.)",
+            date: "2014",
+            marks: "7.3"
+        }
+    ],
+    summary: [
         "A developer with 6+ years of experience in working on digital solutions, primarily based on Angular, React.JS and Android.",
         "Proficiency in delivering quality through performance optimization, following TDD approach and design patterns like MVP and MVVM.",
         "Proactive in giving optimized solution and adhering to industry best practices and coding standards.",
         "Practiced Agile Scrum methodology.",
         "Experienced on CICD. Has worked on tools like Jenkins, SonarQube and GIT."
+    ],
+    skills: [
+        {
+            category: "Web Technologies",
+            list: ["HTML5", "CSS3", "SASS", "Javascript", "Typescript"]
+        },
+        {
+            category: "Framework/Libraries",
+            list: ["Angular 8", "Jasmine with Karma", "ReactJS", "Android"] 
+        },
+        {
+            category: "Tools",
+            list: ["VSCode", "SonarQube", "Jenkins", "Rally", "New Relic"]
+        }
+    ],
+    employment: [
+        {
+            company: "General Electric - Power",
+            location: "Bengaluru",
+            position: "Senior Software Engineer",
+            from: "Sept 2018",
+            to: "Current"
+        },
+        {
+            company: "Infosys Technologies Ltd.",
+            location: "Bengaluru",
+            position: "Technical Analyst",
+            from: "Sept 2014",
+            to: "Sept 2018"
+        }
+    ],
+    projects: [
+        {
+            title: "Modelling Platform",
+            company: "GE Power",
+            description: "A B2B responsive web application used by different roles like Admin and Modeller to create customer models which includes creating customer inventory, outage management, finance and billing. It's a critical application adhering to highest of security and quality standards.",
+            contributions: [
+                "Developed reconfiguration feature which helped in changing part configurations.",
+                "Developed reusable components like confirmation pop-up, group checkbox, common stepper etc.",
+                "Improved code coverage by ~20% bringing overall coverage from 67% to 90%",
+                "Drive best practices and quality standards throughout the various UI PODs",
+                "Mentor junior UI developers",
+                "Review code and PRs"
+            ],
+            techstack: "Angular 8, Jasmine/Karma, NgRx, RxJS, Angular Material, TypeScript, HTML, SASS"
+        },
+        {
+            title: "",
+            company: "",
+            description: "",
+            contributions: [
+                "Developed a whole new module which helped an engineer run solutions and view the output.",
+                "Added role level permissions to different modules.",
+                "",
+                ""
+            ],
+            techstack: ""
+        },
+        {
+            title: "",
+            company: "",
+            description: "",
+            contributions: [
+                "",
+                ""
+            ],
+            techstack: ""
+        },
+        {
+            title: "",
+            company: "",
+            description: "",
+            contributions: [
+                "",
+                ""
+            ],
+            techstack: ""
+        }
+    ],
+    certifications: [
+        {
+            name: "SAFe 5 Practitioner",
+            issuer: "Scaled Agile, Inc.",
+            issueDate: "April 14, 2022",
+            expiryDate: "April 14, 2023"
+        }
+    ],
+    awards: [
+        {
+            company: "General Electric - Power",
+            name: "Impact Award",
+            description: "Awarded for excellence in driving front end team, best practices and quality delivery.",
+            date: "September, 2020"
+        },
+        {
+            company: "General Electric - Power",
+            name: "Impact Award",
+            description: "Awarded for bringing a critical module to stability in production by resolving priority issues.",
+            date: "September, 2019"
+        },
+        {
+            company: "General Electric - Power",
+            name: "Impact Award",
+            description: "Awarded for excellence in delivery with quality",
+            date: "December, 2018"
+        },
+        {
+            company: "AT&T - Infosys Technologies Ltd",
+            name: "Best Rookie",
+            description: "Awarded for excellence in app development and client engagement.",
+            date: "March, 2016"
+        }
     ]
 }
--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function fetchResumeData() {
-    console.log(ajay);
-    return ajay;
+    console.log(ajay)
+    return ajay
 }
 
-function addSummary() {
-    
+function addSummary(summaryPoints) {
+    const summaryDiv = document.getElementById('summary')
+    const ul = document.createElement('ul')
+    summaryPoints.forEach((point) => {
+        const li = document.createElement('li')
+        li.innerHTML = point
+        ul.appendChild(li)
+    })
+    summaryDiv.appendChild(ul)
 }
 
 function renderResume() {
-    const resumeData = fetchResumeData();
-    addSummary(resumeData.summary);
+    const resumeData = fetchResumeData()
+    addSummary(resumeData.summary)
+    addSkills(resumeData.skills)
 }
 
-renderResume();
+renderResume()
